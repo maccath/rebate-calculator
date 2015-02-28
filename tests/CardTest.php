9f453f930e03c6a9b4d771b6de9bcffc91340300
Katy Ereira <kachii@gmail.com>
2015-02-28 08:21:03 +0000

    Add unit tests for Card class.

--- tests/CardTest.php
+++ tests/CardTest.php
@@ -2,12 +2,50 @@
 
 class CardTest extends PHPUnit_Framework_TestCase
 {
-    public function testGetBalance()
-    {
-        // Arrange
-        $a = new \RebateCalculator\Card(new \RebateCalculator\PercentageFee(10), 100);
+    protected $card;
 
-        // Assert
-        $this->assertEquals(100, $a->getBalance());
+    protected function setUp()
+    {
+        // Set up card
+        $this->card = new \RebateCalculator\Card(new \RebateCalculator\PercentageFee(10), 100);
+    }
+
+    public function testBalance()
+    {
+        $this->assertEquals(100, $this->card->getBalance());
+
+        $this->card->setBalance(25);
+
+        $this->assertEquals(25, $this->card->getBalance());
+    }
+
+    public function testMinimumTopup()
+    {
+        $this->assertEquals(0, $this->card->getMinimumTopup());
+
+        $this->card->setMinimumTopup(25);
+
+        $this->assertEquals(25, $this->card->getMinimumTopup());
+    }
+
+    public function testFee()
+    {
+        $this->assertInstanceOf('\RebateCalculator\FeeInterface', $this->card->getFee());
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testTopupException()
+    {
+        $this->card->setMinimumTopup("djkdjlasd");
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testFeeException()
+    {
+        $this->card->setFee("djkdjlasd");
     }
 }
