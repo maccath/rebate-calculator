9a0883437625fbd94ee5d1e260ca7296ad91f6d7
Katy Ereira <kachii@gmail.com>
2015-03-01 18:44:51 +0000

    Add minimum topup values to SavingsCalculatorTest.

--- tests/SavingsCalculatorTest.php
+++ tests/SavingsCalculatorTest.php
@@ -7,6 +7,7 @@ class SavingsCalculatorTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @param \RebateCalculator\FeeInterface    $fee
+     * @param                                   $minimumTopup
      * @param \RebateCalculator\RebateInterface $rebate
      * @param \RebateCalculator\Item            $item
      * @param                                   $expectedCost
@@ -15,12 +16,13 @@ class SavingsCalculatorTest extends PHPUnit_Framework_TestCase
      */
     public function testCalculateCost(
         \RebateCalculator\FeeInterface $fee,
+        $minimumTopup,
         \RebateCalculator\RebateInterface $rebate,
         \RebateCalculator\Item $item,
         $expectedCost
     ) {
         // Set up calculator
-        $topup = new \RebateCalculator\Topup($fee, 0, 0);
+        $topup = new \RebateCalculator\Topup($fee, $minimumTopup);
         $card = new \RebateCalculator\Card($topup);
         $store = new \RebateCalculator\Store("Sainsbury's", $rebate);
         $calculator = new \RebateCalculator\SavingsCalculator($card, $store,
@@ -37,30 +39,35 @@ class SavingsCalculatorTest extends PHPUnit_Framework_TestCase
         return array(
             array(
                 new \RebateCalculator\PercentageFee(10),
+                10,
                 new \RebateCalculator\PercentageRebate(10),
                 new \RebateCalculator\Item(20),
                 20
             ),
             array(
                 new \RebateCalculator\PercentageFee(0),
+                0,
                 new \RebateCalculator\PercentageRebate(10),
                 new \RebateCalculator\Item(20),
                 18
             ),
             array(
                 new \RebateCalculator\FlatFee(2),
+                5,
                 new \RebateCalculator\PercentageRebate(10),
                 new \RebateCalculator\Item(20),
                 20
             ),
             array(
                 new \RebateCalculator\FlatFee(0),
+                10,
                 new \RebateCalculator\PercentageRebate(5),
                 new \RebateCalculator\Item(20),
                 19
             ),
             array(
                 new \RebateCalculator\FlatFee(2),
+                15,
                 new \RebateCalculator\PercentageRebate(10),
                 new \RebateCalculator\Item(15),
                 15.5
