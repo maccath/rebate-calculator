4d3c9e656cf138bfc3b23d5433270fd68b64c5d9
Katy Ereira <kachii@gmail.com>
2015-03-04 06:23:33 +0000

    Throw exceptions on invalid amount/price values for percentage rebates.

--- src/PercentageRebate.php
+++ src/PercentageRebate.php
@@ -31,9 +31,26 @@ class PercentageRebate implements RebateInterface
 
     /**
      * @param $amount
+     *
+     * @throws \Exception
      */
     public function setAmount($amount)
     {
+        if (!$amount) {
+            $this->amount = 0;
+
+            return;
+        }
+
+        if (!is_numeric($amount) || $amount < 0) {
+            throw new \Exception(
+                sprintf(
+                    'Amount (%s) must be a positive numeric value.',
+                    $amount
+                )
+            );
+        }
+
         $this->amount = $amount;
     }
 
@@ -41,9 +58,19 @@ class PercentageRebate implements RebateInterface
      * @param $price
      *
      * @return float
+     * @throws \Exception
      */
     public function calculate($price)
     {
+        if ($price && (!is_numeric($price) || $price < 0)) {
+            throw new \Exception(
+                sprintf(
+                    "Price (%s) must be a positive numeric value.",
+                    $price
+                )
+            );
+        }
+
         return (Float)$price / 100 * (float)$this->amount;
     }
 }
