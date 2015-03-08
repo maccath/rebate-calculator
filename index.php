d49a2e025b2912f10b7221d4b7fe5253df4d8024
Katy Ereira <kachii@gmail.com>
2015-03-08 19:50:13 +0000

    Add calculate action on form submit.
     - Add calculate action on form submit in index.html.
     - Add result.html template to display results of calculation.
     - Add error handling in calculate action in index.php.
     - Refactor calculation functions in index.php.

--- index.php
+++ index.php
@@ -17,58 +17,98 @@ $data = array(
     'application' => $config['application'],
 );
 
-if ($_POST) {
-    // Fetch user input
-    $minimumTopup = $_POST['minimum_topup']
-        ? round(floatval($_POST['minimum_topup']), 2) : 0;
-    $cardBalance = $_POST['current_balance']
-        ? round(floatval($_POST['current_balance']), 2) : 0;
-    $storeName = $_POST['store_name'] ? strip_tags($_POST['store_name'])
-        : 'Unnamed';
-    $rebateAmount = $_POST['percentage_rebate_amount']
-        ? round(floatval($_POST['percentage_rebate_amount']), 2) : 0;
-    $itemCost = $_POST['item_cost'] ? round(floatval($_POST['item_cost']), 2)
-        : 0;
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : false;
 
-    // Construct fee
-    if ($_POST['topup_fee'] == 'flat') {
-        $feeAmount = round(floatval($_POST['flat_fee_amount']), 2);
-        $fee = new \RebateCalculator\FlatFee($feeAmount);
-    } else if ($_POST['topup_fee'] == 'percentage') {
-        $feeAmount = round(floatval($_POST['percentage_fee_amount']), 2);
-        $fee = new \RebateCalculator\PercentageFee($feeAmount);
-    } else {
-        $fee = new \RebateCalculator\FlatFee(0);
+if ($action == 'calculate') {
+    try {
+        if (!($_POST)) {
+            throw new \Exception(sprintf(
+                'No direct access.'
+            ));
+        };
+
+        // Fetch user input
+        $minimumTopup = isset($_POST['minimum_topup'])
+            ? round(floatval($_POST['minimum_topup']), 2)
+            : 0;
+
+        $cardBalance = isset($_POST['current_balance'])
+            ? round(floatval($_POST['current_balance']), 2)
+            : 0;
+
+        $storeName = isset($_POST['store_name'])
+            ? strip_tags($_POST['store_name'])
+            : 'Unnamed';
+
+        $rebateAmount = isset($_POST['percentage_rebate_amount'])
+            ? round(floatval($_POST['percentage_rebate_amount']), 2)
+            : 0;
+
+        $itemCost = isset($_POST['item_cost'])
+            ? round(floatval($_POST['item_cost']), 2)
+            : 0;
+
+        // Construct fee
+        if (isset($_POST['topup_fee']) &&  $_POST['topup_fee'] == 'flat') {
+            $feeAmount =  isset($_POST['flat_fee_amount'])
+                ? round(floatval($_POST['flat_fee_amount']), 2)
+                : 0;
+            $fee = new \RebateCalculator\FlatFee($feeAmount);
+        } else if (isset($_POST['topup_fee']) && $_POST['topup_fee'] == 'percentage') {
+            $feeAmount =  isset($_POST['percentage_fee_amount'])
+                ? round(floatval($_POST['percentage_fee_amount']), 2)
+                : 0;
+            $fee = new \RebateCalculator\PercentageFee($feeAmount);
+        } else {
+            $fee = new \RebateCalculator\FlatFee(0);
+        }
+
+        // Construct topup facility
+        $topup = new \RebateCalculator\Topup($fee, $minimumTopup);
+
+        // Construct card
+        $card = new RebateCalculator\Card($topup, $cardBalance);
+
+        // Construct rebate
+        $rebate = new \RebateCalculator\PercentageRebate($rebateAmount);
+
+        // Construct store
+        $store = new \RebateCalculator\Store($storeName, $rebate);
+
+        // Construct item
+        $item = new \RebateCalculator\Item($itemCost);
+
+        // Calculate savings
+        $calculator = new \RebateCalculator\SavingsCalculator($card, $store, $item);
+
+        $overallCost = $calculator->calculateCost();
+        $topupRequired = $calculator->calculateTopupRequired();
+        $rebate = $calculator->calculateRebateAmount();
+        $remainingBalance = $calculator->calculateRemainingBalance();
+        $saving = $item->getCost() - $overallCost;
+
+        $data = array_merge($data, array(
+            'item' => $item,
+            'store' => $store,
+            'card' => $card,
+            'result' => array(
+                'overallCost' => $overallCost,
+                'topupRequired' => $topupRequired,
+                'rebate' => $rebate,
+                'remainingBalance' => $remainingBalance,
+                'saving' => $saving,
+            ),
+        ));
+    } catch (\Exception $e) {
+        $data['error'] = $e->getMessage();
     }
 
-    // Construct topup facility
-    $topup = new \RebateCalculator\Topup($fee, $minimumTopup);
-
-    // Construct card
-    $card = new RebateCalculator\Card($topup, $cardBalance);
-
-    // Construct rebate
-    $rebate = new \RebateCalculator\PercentageRebate($rebateAmount);
-
-    // Construct store
-    $store = new \RebateCalculator\Store($storeName, $rebate);
-
-    // Construct item
-    $item = new \RebateCalculator\Item($itemCost);
-
-    // Calculate savings
-    $calculator = new \RebateCalculator\SavingsCalculator($card, $store, $item);
-
-    $result['overall_cost'] = $calculator->calculateCost();
-    $result['topupRequired'] = $calculator->calculateTopupRequired();
-    $result['rebate'] = $calculator->calculateRebateAmount();
-    $result['remainingBalance'] = $calculator->calculateRemainingBalance();
-
-    echo json_encode($result);
+    echo $twig->render('result.html',
+        array_merge(array('title' => 'Results'), $data));
 
 } else {
 
-    echo $twig->render('index.html', array_merge(array('title' => 'Calculator'), $data));
+    echo $twig->render('index.html',
+        array_merge(array('title' => 'Calculator'), $data));
 
 }
-
