5d8e893f295047560aae02330b96349a43a6e366
Katy Ereira <kachii@gmail.com>
2015-03-08 09:53:42 +0000

    Add basic calculate functionality.
     - Filter user input.
     - Construct dependencies required for calculation.
     - Display results of calculation.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-include ('vendor/autoload.php');
+include('vendor/autoload.php');
 
 // Set up configuration options
 $config = array();
-include ('config/config.php');
+include('config/config.php');
 if (file_exists('config/local.config.php')) {
-    include ('config/local.config.php');
+    include('config/local.config.php');
 }
 
 // Load Twig templating engine
@@ -17,4 +17,58 @@ $data = array(
     'application' => $config['application'],
 );
 
-echo $twig->render('index.html', array_merge(array('title' => 'Calculator'), $data));
+if ($_POST) {
+    // Fetch user input
+    $minimumTopup = $_POST['minimum_topup']
+        ? round(floatval($_POST['minimum_topup']), 2) : 0;
+    $cardBalance = $_POST['current_balance']
+        ? round(floatval($_POST['current_balance']), 2) : 0;
+    $storeName = $_POST['store_name'] ? strip_tags($_POST['store_name'])
+        : 'Unnamed';
+    $rebateAmount = $_POST['percentage_rebate_amount']
+        ? round(floatval($_POST['percentage_rebate_amount']), 2) : 0;
+    $itemCost = $_POST['item_cost'] ? round(floatval($_POST['item_cost']), 2)
+        : 0;
+
+    // Construct fee
+    if ($_POST['topup_fee'] == 'flat') {
+        $feeAmount = round(floatval($_POST['flat_fee_amount']), 2);
+        $fee = new \RebateCalculator\FlatFee($feeAmount);
+    } else if ($_POST['topup_fee'] == 'percentage') {
+        $feeAmount = round(floatval($_POST['percentage_fee_amount']), 2);
+        $fee = new \RebateCalculator\PercentageFee($feeAmount);
+    } else {
+        $fee = new \RebateCalculator\FlatFee(0);
+    }
+
+    // Construct topup facility
+    $topup = new \RebateCalculator\Topup($fee, $minimumTopup);
+
+    // Construct card
+    $card = new RebateCalculator\Card($topup, $cardBalance);
+
+    // Construct rebate
+    $rebate = new \RebateCalculator\PercentageRebate($rebateAmount);
+
+    // Construct store
+    $store = new \RebateCalculator\Store($storeName, $rebate);
+
+    // Construct item
+    $item = new \RebateCalculator\Item($itemCost);
+
+    // Calculate savings
+    $calculator = new \RebateCalculator\SavingsCalculator($card, $store, $item);
+
+    $result['overall_cost'] = $calculator->calculateCost();
+    $result['topupRequired'] = $calculator->calculateTopupRequired();
+    $result['rebate'] = $calculator->calculateRebateAmount();
+    $result['remainingBalance'] = $calculator->calculateRemainingBalance();
+
+    echo json_encode($result);
+
+} else {
+
+    echo $twig->render('index.html', array_merge(array('title' => 'Calculator'), $data));
+
+}
+
