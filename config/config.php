9751e1724be64b48e62a1a36feae089e75bf49e9
Katy Ereira <kachii@gmail.com>
2015-03-08 07:29:38 +0000

    Add index.php, application configuration files and Twig templates.
     - Add configuration file
     - Load Twig and configuration options in index.php
     - Add template cache and local configuration to .gitignore
      - Add templates/ directory
      - Add index.html template

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = array_merge($config, array(
+    // Array to be passed into Twig_Environment constructor
+    'twig' => array(
+        'cache' => 'cache',
+        'debug' => false,
+    ),
+));
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+include ('vendor/autoload.php');
+
+// Set up configuration options
+$config = array();
+include ('config/config.php');
+if (file_exists('config/local.config.php')) {
+    include ('config/local.config.php');
+}
+
+// Load Twig templating engine
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, $config['twig']);
+
+echo $twig->render('index.html', array());
