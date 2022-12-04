a4a8d6d18321193f1fb3945065f4fdb6ca85b37e
Katy Ereira <katy@skillshare.com>
2022-12-04 20:15:03 +0000

    chore: refactor entrypoint

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+use RebateCalculator\Card;
+use RebateCalculator\FlatFee;
+use RebateCalculator\Item;
+use RebateCalculator\PercentageFee;
+use RebateCalculator\PercentageRebate;
+use RebateCalculator\Store;
+use RebateCalculator\TopUpCalculator;
+use RebateCalculator\TopUpFacility;
+
 include('vendor/autoload.php');
 
 // Set up configuration options
@@ -12,109 +21,68 @@ if (file_exists('config/local.config.php')) {
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, $config['twig']);
 
-$data = array(
+$data = [
     'application' => $config['application'],
-);
-
-$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : false;
-
-if ($action == 'calculate') {
-    try {
-        if (!($_POST)) {
-            throw new \Exception(sprintf(
-                'No direct access.'
-            ));
-        };
-
-        // Fetch user input
-        $minimumTopUp = isset($_POST['minimum_topup'])
-            ? round(floatval($_POST['minimum_topup']), 2)
-            : 0;
-
-        $cardBalance = isset($_POST['current_balance'])
-            ? round(floatval($_POST['current_balance']), 2)
-            : 0;
-
-        $storeName = isset($_POST['store_name'])
-            ? strip_tags($_POST['store_name'])
-            : 'Unnamed';
-
-        $rebateAmount = isset($_POST['percentage_rebate_amount'])
-            ? round(floatval($_POST['percentage_rebate_amount']), 2)
-            : 0;
-
-        $itemCost = isset($_POST['item_cost'])
-            ? round(floatval($_POST['item_cost']), 2)
-            : 0;
-
-        // Construct fee
-        $feeAmount =  isset($_POST['fee_amount'])
-            ? round(floatval($_POST['fee_amount']), 2)
-            : 0;
+];
 
 
-        if (isset($_POST['topup_fee']) && $_POST['topup_fee'] == 'percentage') {
-            $fee = new \RebateCalculator\PercentageFee($feeAmount);
-        } else {
-            $fee = new \RebateCalculator\FlatFee($feeAmount);
-        }
-
-        // Construct top-up facility
-        $topUp = new \RebateCalculator\TopUpFacility($fee, $minimumTopUp);
-
-        // Construct card
-        $card = new RebateCalculator\Card($topUp, $cardBalance);
-
-        // Construct rebate
-        $rebate = new \RebateCalculator\PercentageRebate($rebateAmount);
-
-        // Construct store
-        $store = new \RebateCalculator\Store($storeName, $rebate);
-
-        // Construct item
-        $item = new \RebateCalculator\Item($itemCost);
-
-        // Construct calculator
-        $topUpCalculator = new \RebateCalculator\TopUpCalculator($card, $item);
-
-        $topUpAmount = $topUpCalculator->calculateTopUpRequired();
-
-        $topUpCost = 0;
-        if ($topUpAmount > 0) {
-            $topUpCost = $card->getTopUpCost($topUpAmount);
-            $card->topUp($topUpAmount);
-        }
-
-        $rebateValue = $store->calculateRebateValue($item);
-
-        $card->payFor($item);
-        $card->receiveRebate($item, $store);
-
-        $data = array_merge($data, array(
-            'item' => $item,
-            'store' => $store,
-            'card' => $card,
-            'fee' => $fee,
-            'fee_type' => $_POST['topup_fee'] == 'percentage' ?: 'flat',
-            'result' => array(
-                'topUpCost' => $topUpCost,
-                'topUpRequired' => $topUpAmount,
-                'rebate' => $rebateValue,
-                'previousBalance' => $cardBalance,
-                'remainingBalance' => $card->getBalance(),
-                'saving' => $rebateValue - $topUpCost,
-            ),
-        ));
-    } catch (\Exception $e) {
-        $data['error'] = $e->getMessage();
-    }
-
-    echo $twig->render('result.html',
-        array_merge(array('title' => 'Results'), $data));
-
-} else {
-
-    echo $twig->render('index.html',
-        array_merge(array('title' => 'Calculator'), $data));
-
+// Display the form
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo $twig->render('index.html', array_merge(['title' => 'Calculator'], $data));
+    exit;
 }
+
+
+$minimumTopUp = round(floatval($_POST['minimum_topup'] ?? 0), 2);
+$cardBalance = round(floatval($_POST['current_balance'] ?? 0), 2);
+$storeName = strip_tags($_POST['store_name'] ?? 'Unnamed');
+$rebateAmount = round(floatval($_POST['percentage_rebate_amount'] ?? 0), 2);
+$itemCost = round(floatval($_POST['item_cost'] ?? 0), 2);
+$feeType = in_array($_POST['topup_fee'] ?? '', ['percentage', 'flat']) ? $_POST['topup_fee'] : 'flat';
+$feeAmount = round(floatval($_POST['fee_amount'] ?? 0), 2);
+
+try {
+    $fee = $feeType == 'percentage' ? new PercentageFee($feeAmount) : new FlatFee($feeAmount);
+
+    $topUpFacility = new TopUpFacility($fee, $minimumTopUp);
+
+    $card = new Card($topUpFacility, $cardBalance);
+
+    $rebate = new PercentageRebate($rebateAmount);
+
+    $store = new Store($storeName, $rebate);
+
+    $item = new Item($itemCost);
+
+    $topUpCalculator = new TopUpCalculator($card, $item);
+
+    $topUpAmount = $topUpCalculator->calculateTopUpRequired();
+
+    $topUpCost = $card->getTopUpCost($topUpAmount);
+    $card->topUp($topUpAmount);
+
+    $rebateValue = $store->calculateRebateValue($item);
+
+    $card->payFor($item);
+    $card->receiveRebate($item, $store);
+
+    $data = array_merge($data, [
+        'item' => $item,
+        'store' => $store,
+        'card' => $card,
+        'fee' => $fee,
+        'fee_type' => $feeType,
+        'result' => [
+            'topUpCost' => $topUpCost,
+            'topUpRequired' => $topUpAmount,
+            'rebate' => $rebateValue,
+            'previousBalance' => $cardBalance,
+            'remainingBalance' => $card->getBalance(),
+            'saving' => $rebateValue - $topUpCost,
+        ],
+    ]);
+} catch (Exception $e) {
+    $data['error'] = $e->getMessage();
+}
+
+echo $twig->render('result.html', array_merge(['title' => 'Results'], $data));
